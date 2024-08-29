dd2710c872ed4e04dc80e1bb7940ebe96ebad89d
EmilioTec10 <manalfaro@estudiantec.cr>
2024-08-28 23:08:42 -0600

    Se agregaron la mayoria de palabras reservadas para el leguaje en el analisis lexico

--- Analizador_lexico/prueba.py
+++ Analizador_lexico/prueba.py
@@ -3,13 +3,69 @@
 import ply.lex as lex
 
 #Definición de tokens
-tokens = ['NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE']
+tokens = [
+    'DEF',
+    'PUT',
+    'ADD',
+    'CONUP',
+    'CONDOWN',
+    'CONRIGHT',
+    'CONLEFT',
+    'POS',
+    'POSX',
+    'POSY',
+    'USECOLOR',
+    'DOWN',
+    'UP',
+    'BEGIN',
+    'EQUAL',
+    'AND',
+    'OR',
+    'GREATER',
+    'SMALLER',
+    'SUBSTR',
+    'RANDOM',
+    'MULT',
+    'DIV',
+    'SUM',
+    'PARIZQ',
+    'PARDER',
+    'TRUE',
+    'FALSE',
+    'PUNTOCOMA',
+    'NUMBER']
 
 #Expresiones regulares para tokens simples
-t_PLUS = r'\+'
-t_MINUS = r'\-'
-t_TIMES = r'\*'
-t_DIVIDE = r'\/'
+
+t_DEF = r'Def'
+t_PUT = r'Put'
+t_ADD = r'Add'
+t_CONUP = r'ContinueUp'
+t_CONDOWN = r'ContinueDown'
+t_CONRIGHT = r'ContinueRight'
+t_CONLEFT = r'ContinueLeft'
+t_POS = r'Pos'
+t_POSX = r'PosX'
+t_POSY = r'PosY'
+t_USECOLOR = r'UseColor'
+t_DOWN = r'Down'
+t_UP = r'Up'
+t_BEGIN = r'Beginning;'
+t_EQUAL = r'Equal'
+t_AND = r'And'
+t_OR = r'Or'
+t_GREATER = r'Greater'
+t_SMALLER = r'Smaller'
+t_SUBSTR = r'Substr'
+t_RANDOM = r'Random'
+t_MULT = r'Mult'
+t_DIV = r'Div'
+t_SUM = r'Sum'
+t_PARIZQ = r'\('
+t_PARDER = r'\)'
+t_TRUE = r'True'
+t_FALSE = r'False'
+t_PUNTOCOMA = r';'
 
 #Expresión regular para reconocer números enteros
 def t_NUMBER(t):
@@ -29,7 +85,7 @@ def t_error(t):
 lexer = lex.lex()
 
 #Ejemplo de uso
-data = "3 + 4 * 2"
+data = "ContinueUp 10;"
 lexer.input(data)
 
 #Obtener los tokens reconocidos
